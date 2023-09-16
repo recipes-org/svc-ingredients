ae6844ab0b760f8768ddc5ef4897d99f2f774db1
Sam Thorold <sam.thorold@gmail.com>
2023-09-16 17:52:17 +0100

    Progress to 0.1.0

--- tests/integration/test_create_recipe.py
+++ tests/integration/test_create_recipe.py
@@ -1,3 +1,5 @@
+import uuid
+
 import httpx
 import pytest
 
@@ -6,10 +8,10 @@ from ingredients.domain import Ingredient
 
 @pytest.mark.asyncio
 async def test_create_ingredient() -> None:
-    data = Ingredient.model_validate({"name": "Spinach"})
+    data = Ingredient.model_validate({"name": uuid.uuid4().hex})
     async with httpx.AsyncClient() as client:
         resp = await client.post(
             "http://localhost:8008/v1/ingredients/", json=data.model_dump()
         )
 
-    assert resp.status_code == 201
+    assert resp.status_code == 201, resp.json()
